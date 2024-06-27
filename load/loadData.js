0a8902c01e6e284d3d9c6bf95cd318243a930ce6
v-troisi5 <v.troisi5@studenti.unisa.it>
2024-06-27 13:41:57 +0200

    Caricamento dati di pre-loading e script relativo. Da ultimare: prove e investigatori

---
+++ load/loadData.js
@@ -0,0 +1,96 @@
+const {MongoClient} = require('mongodb');
+const fs = require('fs');
+const { join } = require('./sedes.js');
+
+const mongoUri = "mongodb://127.0.0.1:27017/";
+const dbName = "boscombe";
+
+async function main() {
+
+  const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    console.log("Connected");
+
+    const db = client.db(dbName);
+    
+    //Clienti
+    let collectionName = "clientes";
+    let data = require('./clientes.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    let result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Sedi
+    collectionName = "sedes";
+    data = require('./sedes.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Informatori
+    collectionName = "informatores";
+    data = require('./informatores.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Investigatori
+    collectionName = "investigatores";
+    data = require('./investigatores.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    console.log("Aggiorno i clienti con gli incarichi");
+    currentCollection = db.collection("clientes");
+    data.forEach(async function(c){
+      c.incarichi.forEach((async function(i){
+        await currentCollection.updateMany({ _id: i.clienteID }, {$push: {incarichi: i}});
+      }))
+    });
+    console.log("Aggiornamento clienti terminato");
+
+    //Afferenze
+    collectionName = "afferenzas";
+    data = require('./afferenzas.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Collaborazioni
+    collectionName = "collaboraziones";
+    data = require('./collaboraziones.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Consulenti
+    collectionName = "consulentes";
+    data = require('./consulentes.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+
+    //Prova
+    collectionName = "provas";
+    data = require('./provas.js');
+    db.collection(collectionName).drop();
+    currentCollection = db.collection(collectionName);
+    result = await currentCollection.insertMany(data);
+    console.log(`Inserted: ${result.insertedCount} documents` + ' in ' + collectionName);
+  } 
+  catch(error){
+    console.log(error)
+  }
+}
+
+main()
