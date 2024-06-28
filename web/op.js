c1504659988f73a06d6a94dca94fae48fbdbcc7a
v-troisi5 <v.troisi5@studenti.unisa.it>
2024-06-28 15:29:31 +0200

    Aggiunte OP6, OP7 al front end

--- web/op.js
+++ web/op.js
@@ -14,7 +14,7 @@ function showOp(operation) {
 
 function eseguiOp(operation) {
     if(operation == 'operazione4'){
-        sedeID = document.getElementById('op4sedeID').value;
+        let sedeID = document.getElementById('op4sedeID').value;
 
         fetch(`/boscombe/prove/sedi/` + sedeID)
             .then(response => response.json())
@@ -22,13 +22,30 @@ function eseguiOp(operation) {
             .catch(error => console.error('Errore durante l\'operazione 4', error));
     }
     else if(operation == 'operazione5'){
-        clienteID = document.getElementById('op5clienteID').value;
-        tipoIncarico = document.getElementById('op5tipoIncarico').value;
+        let clienteID = document.getElementById('op5clienteID').value;
+        let tipoIncarico = document.getElementById('op5tipoIncarico').value;
 
         fetch(`/boscombe/clienti/` + clienteID + `/incarichi/` + tipoIncarico)
             .then(response => response.json())
             .then(data => op5FormatTable(data))
             .catch(error => console.error('Errore durante l\'operazione 5', error));
+    }
+    else if(operation == 'operazione6'){
+        let testPrimario = document.getElementById('op6testPrimario').value;
+        let dataInsediamento = document.getElementById('op6dataInsediamento').value;
+
+        fetch(`/boscombe/sedi/prove/test/` + testPrimario + `/` + dataInsediamento)
+            .then(response => response.json())
+            .then(data => op6FormatTable(data))
+            .catch(error => console.error('Errore durante l\'operazione 6', error));
+    }
+    else if(operation == 'operazione7'){
+        let costoOrario = document.getElementById('op7costoOrario').value;
+
+        fetch(`/boscombe/informatori/incarichi/` + costoOrario)
+            .then(response => response.json())
+            .then(data => op7FormatTable(data))
+            .catch(error => console.error('Errore durante l\'operazione 7', error));
     }
 }
 
@@ -67,5 +84,81 @@ function op5FormatTable(data) {
             cell.textContent = elem.sedeID._id;
             cell = row.insertCell();
             cell.textContent = elem.sedeID.nome;
+    });
+}
+
+function op6FormatTable(data) {
+    console.log(data)
+    const table = document.getElementById(`operazione6-table`).getElementsByTagName('tbody')[0];
+
+    data.result.forEach(
+        function(elem){
+            const row = table.insertRow();
+
+            let cell = row.insertCell();
+            cell.textContent = elem.sedeID._id;
+            cell = row.insertCell();
+            cell.textContent = elem.sedeID.nome;
+            cell = row.insertCell();
+            cell.textContent = elem.sedeID.via;
+            cell = row.insertCell();
+            cell.textContent = elem.sedeID.civico;
+    });
+}
+
+function op7FormatTable(data) {
+    console.log(data)
+    const table = document.getElementById(`operazione7-table`).getElementsByTagName('tbody')[0];
+
+    data.informatori.forEach(
+        function(elem){
+            const row = table.insertRow();
+
+            let cell = row.insertCell();
+            cell.textContent = elem.informatoreID._id;
+            cell = row.insertCell();
+            cell.textContent = elem.informatoreID.codice_fiscale;
+            cell = row.insertCell();
+            cell.textContent = elem.informatoreID.nome;
+            cell = row.insertCell();
+            cell.textContent = elem.informatoreID.cognome;
+            cell = row.insertCell();
+            cell.textContent = elem.informatoreID.paga;
+            cell = row.insertCell();
+            cell.textContent = elem.informatoreID.descrizione;
+            
+            //Sotto-tabella contatti
+            cell = row.insertCell();
+
+            let contattiTable = document.createElement('table');
+
+            let contattiTableTHead = document.createElement('thead');
+            let contattiTableTopRow = document.createElement('tr');
+
+            let contattiIDContattoHeader = document.createElement('th');
+            contattiIDContattoHeader.appendChild(document.createTextNode('ID contatto'));
+            let contattiTipoContattoHeader = document.createElement('th');
+            contattiTipoContattoHeader.appendChild(document.createTextNode('Tipo contatto'));
+            
+            contattiTableTopRow.appendChild(contattiIDContattoHeader);
+            contattiTableTopRow.appendChild(contattiTipoContattoHeader);
+
+            contattiTableTHead.appendChild(contattiTableTopRow);
+
+            let contattiTableTBody = document.createElement('tbody');
+            contattiTable.appendChild(contattiTableTHead);
+            contattiTable.appendChild(contattiTableTBody);
+
+            cell.appendChild(contattiTable);
+
+            elem.informatoreID.contatti.forEach(
+                function(contatto){
+                    let contattiRow = contattiTableTBody.insertRow();
+                    contattiCell = contattiRow.insertCell();
+                    contattiCell.textContent = contatto.id_contatto;
+                    contattiCell = contattiRow.insertCell();
+                    contattiCell.textContent = contatto.tipo_contatto;
+                }
+            )
     });
 }
